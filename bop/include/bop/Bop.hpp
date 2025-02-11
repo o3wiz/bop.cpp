ddbf3aa35e52218c8bff2d9a8dea1d4113f1401f
Daniel Aviv <danielav@magnusmetal.com>
2025-02-11 11:05:19 +0200

    added concepts constraints add use std::move function for optimization

--- bop/include/bop/Bop.hpp
+++ bop/include/bop/Bop.hpp
@@ -10,14 +10,19 @@ template <typename Op> struct BinFunctor
 	template <typename T, class Proj = std::identity> //
 	constexpr auto operator()(const T &rhs, Proj proj) const
 	{
-		return [&rhs, proj](const auto &lhs) { return Op{}(std::invoke(proj, lhs), rhs); };
+		return [&rhs, proj = std::move(proj)]<typename U>(const U &lhs)
+			requires std::invocable<Proj, const U &> && //
+					 std::invocable<Op, std::invoke_result_t<Proj, const U &>, const T &>
+		{ return Op{}(std::invoke(proj, lhs), rhs); };
 	}
 
 	template <class Proj = std::identity> //
 	constexpr auto operator()(Proj proj) const
 	{
-		return
-			[proj](const auto &lhs, const auto &rhs) { return Op{}(std::invoke(proj, lhs), std::invoke(proj, rhs)); };
+		return [proj = std::move(proj)]<typename T>(const T &lhs, const T &rhs)
+			requires std::invocable<Proj, const T &> && //
+					 std::invocable<Op, std::invoke_result_t<Proj, const T &>, std::invoke_result_t<Proj, const T &>>
+		{ return Op{}(std::invoke(proj, lhs), std::invoke(proj, rhs)); };
 	}
 };
 
